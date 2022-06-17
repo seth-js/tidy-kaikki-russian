daf73ace35cfc6756c734cc326c7dc473e16162a
seth-js <83692925+seth-js@users.noreply.github.com>
2022-06-17 16:14:35 -0500

    Update extract-language.py
    Removed forgotten language specifier and made it output ru-wikiextract.json instead. The next script is expecting that file. Also gave a little more info to update the user on what the script's doing, since this script takes the longest to run.

--- Step 1/extract-language.py
+++ Step 1/extract-language.py
@@ -1,24 +1,32 @@
 import json
 import fileinput
 
-lang = "Russian"
+print("Reading raw-wiktextract-data.json...")
 
 output = []
 
 for line in fileinput.input(
     "raw-wiktextract-data.json", openhook=fileinput.hook_encoded("utf-8")
 ):
-    if f'"lang": "{lang}"' in line:
+    if '"lang": "Russian"' in line:
         output.append(line.strip())
+
+print("Finished reading raw-wiktextract-data.json.")
+
+print("Setting up data for ru-wikiextract.json...")
 
 json_file = []
 
+index = 0
 for entry in output:
+    index += 1
+    if index == len(output) or index == 1 or index % 1000 == 0:
+        print(f"{index}/{len(output)} entries added...")
     json_file.append(json.loads(entry))
 
-print(f"Writing {lang.lower()}-wikiextract.json...")
+print("Writing ru-wikiextract.json...")
 
-with open(f"{lang.lower()}-wikiextract.json", "w", encoding="utf-8") as f:
+with open("ru-wikiextract.json", "w", encoding="utf-8") as f:
     json.dump(json_file, f)
 
 print("Finished.")
